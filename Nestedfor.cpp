da5ca0e669898a12355be457feb7709c684d38bd
Rafli Ghozali Alfaina <alfaina25@gmail.com>
2017-02-25 03:06:46 +0000

    Program dari E-Learning
    Saya mencoba dengan referensi dari internet tapi saya berusaha memahaminya agar tidak cupa copas :)

---
+++ Nestedfor.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+//I.S Program menunggu memasukan data
+//F.S Program menampilkan Data sesuai operasi logika
+
+int main () {
+	//Kamus
+	int a, b;
+
+	//Mencari Nilai Prima Menggunakan For
+	for(a = 2; a<100; a++) {
+      for(b = 2; b <= (a/b); b++)
+         if(!(a%b)) break; 
+         if(b > (a/b)) cout << a << " Adalah Nilai Prima" << endl;
+   }
+
+	//Perintah mengakhiri program
+	return 0;
+}

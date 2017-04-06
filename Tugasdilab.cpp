fd5fa6a56369858dd300381f2744335fabbe0894
Rafli Ghozali Alfaina <alfaina25@gmail.com>
2017-04-06 07:30:13 +0700

    Program Pencarian Min & Max (Beta Versi)
    Sudah bisa mencari total dari min/max, kekurangan belum menggunakan (Void PilihanA()/PilihanB)

--- Tugasdilab.cpp
+++ Tugasdilab.cpp
@@ -1,41 +1,81 @@
 #include <iostream>
 using namespace std;
+//I.S Program menunggu user memasukan data
+//F.S Program menampilkan nilai Max/Min beserta jumlah kemunculanya
 
 int main () {
-	int N,p,min,max;
-	char r;
-
 	//Kamus
+	int N,p;
+	char r;
+	//Meminta user untuk memasukan jumlah N
 	cout << "Masukan Berapa Banyak N : ";
 	cin >> p;
-	if (p > 0){
+	//Agar user memasukan angka di atas -1
+	if (p >= 0){
 		N = p;
 
 	} else {
+		//Memunculkan notif Nilai tidak positif dan menghentikan program
 		cout << "Nilai Tidak Positif";
 		return 0;
 	}
+	//Meminta user memilih program yang akan di jalankan A = min, B = Max
+	cout << endl << "A = Mencari Min, B = Mencari Nilai Max" << endl << endl;
 	cout << "Masukan Pilihan Program A/B : ";
+	// r untuk menyimpan pilihan user
 	cin >> r;
-	int angka [100];
-	min = angka[0];
-	max = angka[0];
-	for (int i = 1, l = 0; i <= p; i++, l++){
-		cout << "Masukan Nilai ke " << i << " : ";
-		cin >> angka[l];
 
-		if (min > angka[l]){
-			min = angka[l];
-		}
-		if (max < angka[l]){
-			max = angka[l];
-		}
-	}
+	//kamus
+	//Membuat array
+	int angka[N];
+	int m, n = 0,t = 0;
+
+//Jika Pilihan A (Min)
 	if (r == 'A'){
-		cout << "Nilai Min = " << min;
-	} else if (r == 'B'){
-		cout << "Nilai Max = " << max;
+		//Pengulangan untuk meminta input dari user berulang sebanyak N
+		for (int i = 0 ; i < N; i++){
+		cout << "Masukan Nilai ke " << i+1 << " : ";
+		cin >> angka[i];
+		//Rumus Menentukan Nilai Min
+		if (m > angka[i]){
+			m = angka[i];
+}
+
+}		for (int l = 0; l < N; l++){
+		//Rumus untuk menentukan banyaknya kemunculan nilai Min
+		if (m == angka[l]){
+			t++;
+		}
+		}
+
+		//Menampilkan Hasil
+		cout << "Nilai Min = " << m << endl;
+		cout << "Jumlah Kemunculan = " << t;
+		
+	
+//Jika Pilihan B (MAx)
+} 		else if (r == 'B'){
+	//Pengulangan untuk meminta input dari user berulang sebanyak N
+		for (int i = 0 ; i < N; i++){
+		cout << "Masukan Nilai ke " << i+1 << " : ";
+		cin >> angka[i];
+		//Rumus Menentukan Nilai Max
+		if (n < angka[i]){
+			n = angka[i];
+}
+
+} 		for (int l = 0; l < N; l++){
+		//Rumus untuk menentukan banyaknya kemunculan nilai Max
+		if (n == angka[l]){
+			t++;
+		}
+		}
+
+		//Menampilkan Hasil
+		cout << "Nilai Max = " << n << endl;
+		cout << "Jumlah Kemunculan = " << t;
 	}
+	//Menutup Program
 	return 0;
 }
 
